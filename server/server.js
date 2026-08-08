79c03f0a6c419f9ceafb04f536a93f1b7b97a5f6
Soham Vibhandik <sohamsonar71@gmail.com>
2026-08-08 23:47:14 +0530

    feat: complete backend routes and core functionality

--- server/server.js
+++ server/server.js
@@ -4,10 +4,42 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { Server } from 'socket.io'
+
 
 //create express app and http server
 const app = express();
 const server = http.createServer(app);
+
+//Initialize Socket.io server
+export const io = new Server(server, {
+    cors : {origin: "*"}
+})
+
+//store Online users 
+export const userSocketMap = {} // {userId : socketId}
+
+//Socket.io connection event
+io.on("connection", (socket) => {
+
+    const userId = socket.handshake.query.userId;
+
+    console.log("user connected.", userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    //Emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers",Object.keys(userSocketMap))
+    })
+    
+})
+
 
 //middleware setup
 app.use(express.json({"limit" : "4mb"}));
@@ -19,6 +51,9 @@ app.use("/api/status", (req,res) => (
 
 //userRouter is mounted as middleware on /api/auth.
 app.use("/api/auth", userRouter)
+
+//messageRouter is mounted as middleware on /api/messages.
+app.use("/api/messages", messageRouter);
 
 //connect Database
 await connectDB();
